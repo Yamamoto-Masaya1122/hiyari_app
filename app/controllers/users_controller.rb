8898f3d8deca2d8f3d8ea8611b607cd7bfc3a096
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-07-28 20:48:01 +0900

    ログイン機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: %i[create new]
 
-  def index; end
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
@@ -9,9 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, success: 'User was successfully created.'
+      redirect_back_or_to root_path, success: t('.success')
     else
-      flash.now[:danger] = 'User creation failed.'
+      flash.now[:error] = t('.fail')
       render :new, status: :unprocessable_entity
     end
   end
