3c8a30ff01decf6f8c297f1b6d77461ab2ce531e
Masaya Yamamoto <masaya1122baseball@gmail.com>
2023-07-30 17:13:33 +0900

    Add: Postコントローラーを作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   resources :users, only: [:index, :new, :create]
+  resources :posts
 end
